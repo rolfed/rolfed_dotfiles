96da199f95de9ba78151192b2524af0fa6d46703
Danniel Rolfe <danniel.rolfe@experian.com>
2024-07-16 14:14:39 -0700

    Refactor telescope git files search

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,7 +3,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>FF', builtin.find_files, {})
 
 -- git specific files
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fF', builtin.git_files, {})
 
 -- find buffers
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
