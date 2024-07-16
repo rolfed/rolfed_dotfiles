b9b71e69d645decd9a47b4aaceb511908b160450
Danniel Rolfe <danniel.rolfe@experian.com>
2024-07-16 13:57:57 -0700

    Implement noice

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#908caa' })
-vim.api.nvim_set_hl(0, 'LineNr', { fg = '#f6c177' })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#908caa' })
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#908caa' })  
+vim.api.nvim_set_hl(0, 'LineNr', { fg = '#f6c177' })  
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#908caa' })  
 
 vim.cmd("colorscheme rose-pine")
 -- vim.cmd("colorscheme rose-pine-main")
