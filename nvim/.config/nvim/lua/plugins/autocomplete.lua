474b5b8e8a52dca6f4b37f1c0132059f2520ec7e
Danniel Rolfe <danniel.rolfe@experian.com>
2024-07-26 13:04:15 -0700

    Add indent line

--- nvim/.config/nvim/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -56,7 +56,7 @@ return {
                 },
                 mapping = cmp.mapping.preset.insert(cmp_mappings),
                 sources = cmp.config.sources({
-                    { name = "nvim_lsp", group_index = 1 },     -- lsp
+                    { name = "nvim_lsp", group_index = 1 },                     -- lsp
                     { name = "buffer",   max_item_count = 5, group_index = 2 }, -- text within current buffer
                     { name = "path",     max_item_count = 3, group_index = 3 }, -- file system paths
                     { name = "luasnip",  max_item_count = 3, group_index = 5 }, -- snippets
