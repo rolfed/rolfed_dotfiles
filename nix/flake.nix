eef2d3cb137bf2c23285732848d5a0b18150c34b
Danniel Rolfe <talkto@dannielrolfe.com>
2024-11-06 11:41:12 -0800

    Add darwin nix config

---
+++ nix/flake.nix
@@ -0,0 +1,106 @@
+{
+  description = "Rolfe Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  let
+    configuration = { pkgs, config, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+          # neovim - current issue https://github.com/wez/wezterm/issues/5990
+          bat
+          eza
+          fzf
+          git
+          neovim
+          raycast
+          starship
+          stow
+          tmux
+          zoxide
+          cargo
+      ]; 
+
+      homebrew = {
+        enable = true;
+      }; 
+      fonts.packages = with pkgs; [
+          (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+        ];
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Danniels-Mac-Pro
+    darwinConfigurations."Danniels-Mac-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration 
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = { 
+              enable = true; 
+              # Apple Silicon Only
+              # enableRosetta = true
+              user = "dannielrolfe";
+              # Automatically migrate existing Homebrew installations
+              autoMigrate = true;
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."Danniels-Mac-Pro".pkgs;
+  };
+}
+
