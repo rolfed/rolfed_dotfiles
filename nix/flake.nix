a682d2dd2cbc7ba9b32f19d0a2ca22d18b0083e8
Danniel Rolfe <talkto@dannielrolfe.com>
2024-11-07 16:39:03 -0800

    Add system defaults

--- nix/flake.nix
+++ nix/flake.nix
@@ -63,11 +63,6 @@
           (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       ];
 
-      # MacOs System defaults
-      system.defaults = {
-        dock.autohide = true;
-      };
-
       system.activationScripts.applications.text = let
         env = pkgs.buildEnv {
           name = "system-applications";
@@ -81,12 +76,22 @@
         rm -rf /Applications/Nix\ Apps
         mkdir -p /Applications/Nix\ Apps
         find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
+        while read -r src; do
           app_name=$(basename "$src")
           echo "copying $src" >&2
           ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
         done
             '';
+
+      # MacOs System defaults
+      system.defaults = {
+        dock.autohide = true;
+        dock.persistent-apps = [
+            "${pkgs.wezterm}/Applicatations/Wezterm.app"
+        ];
+        finder.FXPreferredVidwStyle = "clmv";
+        loginwindow.GuestEnabled = false;
+      };
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
