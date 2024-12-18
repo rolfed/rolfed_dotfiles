56b34ad2302660b7eeb068e8905b1826d6bb51a3
Danniel Rolfe <danniel.rolfe@experian.com>
2024-12-18 15:10:17 -0800

    Move extentions into an array

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -15,10 +15,10 @@ return {
     require('telescope').setup {
       extensions = {
         fzf = {
-          fuzzy = true,               -- false will only do exact matching
+          fuzzy = true,                   -- false will only do exact matching
           override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = "smart_case",   -- or "ignore_case" or "respect_case"
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
           -- the default case_mode is "smart_case"
         },
         ['ui-select'] = {
@@ -29,8 +29,14 @@ return {
       }
     }
 
-    require('telescope').load_extension('ui-select')
-    require('telescope').load_extension('notify')
-    require('telescope').load_extension('live_grep_args')
+    local loadExtentions = {
+      'ui-select',
+      'notify',
+      'live_grep_args'
+    }
+
+    for _, value in pairs(loadExtentions) do
+      require('telescope').load_extension(value)
+    end
   end
 }
