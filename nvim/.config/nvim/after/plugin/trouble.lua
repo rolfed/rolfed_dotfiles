ba9c88b6a177a03fb20d8a643291582bdbb98712
Danniel Rolfe <danniel.rolfe@experian.com>
2024-12-18 10:10:29 -0800

    Add trouble keymap descriptions

---
+++ nvim/.config/nvim/after/plugin/trouble.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<leader>xx', '<cmd>Trouble<cr>', { desc = 'Trouble diagnostics toggle'})
+vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Buffer diagnostics (Trouble)'})
+vim.keymap.set('n', '<leader>xq', '<cmd>Trouble qflist toggle<cr>', { desc = 'Quickfix list (Troulbe)'})
