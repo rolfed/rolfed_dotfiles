09b3e45cab52b9dae29c8f4523e2baf3e119c3a5
Danniel Rolfe <danniel.rolfe@experian.com>
2024-12-18 09:51:05 -0800

    Add obsidian key mappings

--- nvim/.config/nvim/after/plugin/obsidian.lua
+++ nvim/.config/nvim/after/plugin/obsidian.lua
@@ -1,51 +1,56 @@
+vim.keymap.set('n', '<leader>nn', '<cmd>ObsidianNew<cr>')
+vim.keymap.set('n', '<leader>nd', '<cmd>ObsidianDailes<cr>')
+vim.keymap.set('n', '<leader>ns', '<cmd>ObsidianSearch<cr>')
+vim.keymap.set('n', '<leader>nw', '<cmd>ObsidianWorkspace<cr>')
+
 require("obsidian").setup({
-    workspaces = {
-        {
-            name = "Work",
-            path = "~/repos/vault/work"
-        },
-        {
-            name = "Personal",
-            path = "~/repos/vault/personal"
-        }
+  workspaces = {
+    {
+      name = "Work",
+      path = "~/repos/vault/work"
     },
-    daily_notes = {
-        -- Optional, if you keep daily notes in a separate directory.
-        folder = "notes/dailies",
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = "%Y-%m-%d",
-        -- Optional, if you want to change the date format of the default alias of daily notes.
-        alias_format = "%B %-d, %Y",
-        -- Optional, default tags to add to each new daily note created.
-        default_tags = { "daily-notes" },
-        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = nil
-    },
+    {
+      name = "Personal",
+      path = "~/repos/vault/personal"
+    }
+  },
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = "notes/dailies",
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    date_format = "%Y-%m-%d",
+    -- Optional, if you want to change the date format of the default alias of daily notes.
+    alias_format = "%B %-d, %Y",
+    -- Optional, default tags to add to each new daily note created.
+    default_tags = { "daily-notes" },
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = nil
+  },
 
-    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-    completion = {
-        -- Set to false to disable completion.
-        nvim_cmp = true,
-        -- Trigger completion at 2 chars.
-        min_chars = 2,
-    },
+  -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+  completion = {
+    -- Set to false to disable completion.
+    nvim_cmp = true,
+    -- Trigger completion at 2 chars.
+    min_chars = 2,
+  },
 
-    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
-    -- way then set 'mappings = {}'.
-    mappings = {
-        -- Toggle check-boxes.
-        ["<leader>ch"] = {
-            action = function()
-                return require("obsidian").util.toggle_checkbox()
-            end,
-            opts = { buffer = true },
-        },
-        -- Smart action depending on context, either follow link or toggle checkbox.
-        ["<cr>"] = {
-            action = function()
-                return require("obsidian").util.smart_action()
-            end,
-            opts = { buffer = true, expr = true },
-        }
+  -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+  -- way then set 'mappings = {}'.
+  mappings = {
+    -- Toggle check-boxes.
+    ["<leader>ch"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
     },
+    -- Smart action depending on context, either follow link or toggle checkbox.
+    ["<cr>"]       = {
+      action = function()
+        return require("obsidian").util.smart_action()
+      end,
+      opts = { buffer = true, expr = true },
+    }
+  },
 })
